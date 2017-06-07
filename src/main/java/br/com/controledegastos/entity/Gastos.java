d6557c6f16a2d8a9acb91199ba28967c0db2c16c
ViniciusBezerra94 <vinicius_bezerra@hotmail.com>
2017-06-07 19:38:18 -0300

    adicionado formualrio de cadastro

---
+++ src/main/java/br/com/controledegastos/entity/Gastos.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.controledegastos.entity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author vinicius
+ */
+public class Gastos {
+    private String descricao;
+    private Date data;
+    private double valor;
+    private String tipoGasto;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getTipoGasto() {
+        return tipoGasto;
+    }
+
+    public void setTipoGasto(String tipoGasto) {
+        this.tipoGasto = tipoGasto;
+    }
+    
+    
+    
+    
+}
